955b65a3c3bd9197e872f69c02b41d781c3dde17
manojkunapalli <manojkunapalli@gmail.com>
2020-09-29 12:07:24 +0530

    added: computed a % b + c

--- sortArithmetic.sh
+++ sortArithmetic.sh
@@ -9,7 +9,9 @@ read -p "enter c = " c
 result=$(( $a + $b * $c ))
 result2=$(( $a * $b + $c ))
 result3=$(( $c + $a / $b ))
+result4=$(( $a % $b + $c ))
 
 echo "a + b * c = $result"
 echo "a * b + c = $result2"
 echo "c + a / b = $result3"
+echo "a % b + c = $result4"
