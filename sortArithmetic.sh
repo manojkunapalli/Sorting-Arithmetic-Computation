b5f8f45511e9fee8866c3e5a37ac2a0d4e219059
manojkunapalli <manojkunapalli@gmail.com>
2020-09-29 12:00:27 +0530

    added: computed c + a / b

--- sortArithmetic.sh
+++ sortArithmetic.sh
@@ -8,6 +8,8 @@ read -p "enter c = " c
 
 result=$(( $a + $b * $c ))
 result2=$(( $a * $b + $c ))
+result3=$(( $c + $a / $b ))
 
 echo "a + b * c = $result"
 echo "a * b + c = $result2"
+echo "c + a / b = $result3"
